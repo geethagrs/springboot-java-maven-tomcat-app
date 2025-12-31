58d842a92caa665e2314c980aaac9f6eb8fa4c5e
root <root@ip-172-31-24-79.ec2.internal>
2025-12-31 17:09:30 +0000

    istudios devops project

---
+++ src/main/java/com/javatpoint/spring_boot_war_deployment_example/SpringBootWarDeploymentExampleApplication.java
@@ -0,0 +1,13 @@
+package com.javatpoint.spring_boot_war_deployment_example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWarDeploymentExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWarDeploymentExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/javatpoint/spring_boot_war_deployment_example/SpringBootWarDeploymentExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javatpoint.spring_boot_war_deployment_example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWarDeploymentExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
